545e563765b9965a2b5401d0b14b774a40173c66
Aaron Lee <aaronlab.net@gmail.com>
2021-09-25 21:18:09 +0900

    init controller

--- first-step/src/cats/cats.controller.ts
+++ first-step/src/cats/cats.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  @Post()
+  @Header('Cache-Control', 'none')
+  create(): string {
+    return 'This action adds a new cat.';
+  }
+
+  @Get('docs')
+  @Redirect('http://aaronlab.net', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://github.com/aaronLab' };
+    }
+  }
+
+  @Get()
+  findAll(@Req() request: Request): string {
+    console.log(request);
+    return 'This action returns all cats';
+  }
+
+  @Get('ab*cd')
+  wildCard(): string {
+    return 'This route uses a wildcard';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id}`;
+  }
+}
